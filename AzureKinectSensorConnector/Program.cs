34bae43c09fcbec45c6d13f34d6ee9d51b87e310
Alexander Beck <github@beck-alexander.de>
2019-11-20 17:33:18 +0100

    Refactoring done

--- AzureKinectSensorConnector/Program.cs
+++ AzureKinectSensorConnector/Program.cs
@@ -20,7 +20,6 @@ namespace AzureKinectSensorConnector
             try
             {
                 using (Device kinect = Device.Open(0))
-
                 {
                     Console.WriteLine("Device Serial Number: " + kinect.SerialNum);
                     kinect.StartCameras(new DeviceConfiguration
@@ -35,7 +34,7 @@ namespace AzureKinectSensorConnector
                     int colorWidth = kinect.GetCalibration().ColorCameraCalibration.ResolutionWidth;
                     int colorHeight = kinect.GetCalibration().ColorCameraCalibration.ResolutionHeight;
 
-                    // Allocate image buffers for us to manipulate
+                    // Start Picture loop
                     using (Image transformedDepth = new Image(ImageFormat.Depth16, colorWidth, colorHeight))
                     using (Image outputColorImage = new Image(ImageFormat.ColorBGRA32, colorWidth, colorHeight))
                     using (Transformation transform = kinect.GetCalibration().CreateTransformation())
@@ -89,66 +88,11 @@ namespace AzureKinectSensorConnector
 
                             rgbColorBitmap.Save(@"C:\temp\kinect_rgb.bmp", System.Drawing.Imaging.ImageFormat.Bmp);
                             depthColorBitmap.Save(@"C:\temp\kinect_depth.bmp", System.Drawing.Imaging.ImageFormat.Bmp);
-
-                            // Console.WriteLine(inputColorBitmap.PixelWidth);
-
-                            // var newImage = System.Drawing.Image. .FromFile("SampImag.jpg");
                         }
-
-                        //    using (Capture capture = Sensor.Capture.Create())
-                        //{
-                        //    Image colorImage = capture.Color;
-
-                        //    var format = colorImage.Format;
-
-                        //    int colorWidth = 1920, colorHeight = 1080;
-                        //    Image colorImage = new Image(format, colorWidth, colorHeight, colorWidth * 4);
-
-                        //    colorImage. .CopyBytesFrom(btColorImage, 0, 0, btColorImage.Length);
-                        //    colorImage.Timestamp = new TimeSpan(DateTime.Now.Ticks);
-
-
-                        //    capture.Color = colorImage;
-                        //}
                     }
+                    //End Picture Mode
 
                 }
-                //Fancy stuf
-                //                //RenderTargetBitmap renderBitmap = new RenderTargetBitmap(200, 250, 300, 300, PixelFormats.Pbgra32);
-                //                DrawingVisual dv = new DrawingVisual();
-                //                using (DrawingContext dc = dv.RenderOpen())
-                //                {
-                //                    VisualBrush brush = new VisualBrush();
-                //                    dc.DrawRectangle(brush, null, new System.Windows.Rect(new System.Windows.Point(), new System.Windows.Size(200, 250)));
-
-                //                }
-
-
-                ////                renderBitmap.Render(dv);
-
-                //                BitmapEncoder encoder = new PngBitmapEncoder();
-
-                //                string time = System.DateTime.Now.ToString("hh'-'mm'-'ss", CultureInfo.CurrentUICulture.DateTimeFormat);
-                //                string myPhotos = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
-                //                string path = Path.Combine(myPhotos, "KinectScreenshot-" + time + ".png");
-
-                //                try
-                //                {
-                //                    using (FileStream fs = new FileStream(path, FileMode.Create))
-                //                    {
-                //                        encoder.Save(fs);
-                //                    }
-                //                    Console.WriteLine("Saved encoder");
-                //                }
-                //                catch (Exception e)
-                //                {
-                //                    Console.WriteLine("MIST: " + e.Message);
-                //                    throw;
-                //                }
-
-
-                //                kinect.Dispose();
-
             }
             catch (Exception e)
             {
@@ -157,9 +101,6 @@ namespace AzureKinectSensorConnector
 
             Console.WriteLine("Finished");
             Console.ReadLine();
-
-
-
         }
     }
 }
